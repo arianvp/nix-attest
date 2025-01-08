a47ba42737598b000d1e73ef25358c25793662d9
Arian van Putten <arian.vanputten@gmail.com>
2025-01-08 17:30:37 +0100

    try fix flake and try attestation

--- flake.nix
+++ flake.nix
@@ -1,17 +1,31 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   nixConfig.post-build-hook = ./hook.sh;
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+    in
+    {
 
-    packages.aarch64-linux = with nixpkgs.legacyPackages.aarch64-linux; {
-      example = runCommand "example" {
-        outputs = [ "bin" "lib" "out" ];
-        buildInputs = [ hello ];
-      } ''
-        hello > $out
-        hello > $bin
-        hello > $lib
-      '';
+      packages = lib.genAttrs lib.systems.flakeExposed (
+        system: with nixpkgs.legacyPackages.${system}; {
+          example =
+            runCommand "example"
+              {
+                outputs = [
+                  "bin"
+                  "lib"
+                  "out"
+                ];
+                buildInputs = [ hello ];
+              }
+              ''
+                hello > $out
+                hello > $bin
+                hello > $lib
+              '';
+        }
+      );
     };
-  };
 }
