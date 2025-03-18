e48aef71d2030be39bbb32ba4c6c7198cea1cc19
Arian van Putten <arian.vanputten@gmail.com>
2025-03-18 16:15:30 +0000

    fixes

--- nix-attest-post-build-hook.nix
+++ nix-attest-post-build-hook.nix
@@ -2,7 +2,10 @@
   writeShellApplication,
   jq,
   nix,
+  python3Packages,
 }:
+
+
 writeShellApplication {
   name = "nix-attest-post-build-hook";
   runtimeInputs = [
@@ -11,34 +14,29 @@ writeShellApplication {
   ];
   text = ''
     export IFS=' '
-    env
-
-    # DRV_PATH and OUT_PATHS
-    # DRV_PATH=/nix/store/4hhj43gzgvzarwc1jl5kgj7dq31mx1zv-example.drv
-    # OUT_PATHS="/nix/store/bfqvkn7yp29dpsbp7hssmvllmd2gin7x-example-bin /nix/store/c07lm8hpqamcxnyzyc541p1f62325s2p-example /nix/store/gpbijimi8kdn4afjd62la158pbhb0iq4-example-lib"
-
-
+  
     # We add both narHash (which in-toto does not define) and sha256 (which in-toto does define)
     # the sha256 is over the nar-serialization. So people should do `nix store dump-path` as file to verify instead of a directory
 
     subjects=$(for path in $OUT_PATHS; do
-        subject=$(nix path-info "$path" --json | jq --arg path "$path" '.[$path]| {name:$path, digest:{narHash:.narHash}, annotations:.}')
+        subject=$(nix path-info "$path" --json | jq --arg path "$path" '.[$path]| {name:$path, digest:{narHash:.narHash}}')
         narHash=$(echo "$subject" | jq -r '.digest.narHash')
         sha256="$(nix hash convert --to base16 "$narHash")"
         echo "$subject" | jq --arg sha256 "$sha256" '.digest.sha256=$sha256'
     done  | jq -s .)
     derivation=$(nix derivation show "$DRV_PATH" | jq --arg DRV_PATH "$DRV_PATH" '.[$DRV_PATH]')
+
+    # TODO: For nix substitution we do not want to have build time dependencies but runtime dependencies
+
     inputDrvs=$(jq -r -n --argjson derivation "$derivation" '$derivation.inputDrvs | to_entries | map(.key as $key | .value.outputs[]|"\($key)^\(.)")[]')
     inputSrcs=$(jq -r -n --argjson derivation "$derivation" '$derivation.inputSrcs[]')
 
     resolvedDependencies=$({ echo "$inputDrvs"; echo "$inputSrcs"; } | while IFS= read -r path; do
-        dep=$(nix path-info "$path" --json | jq 'to_entries | map({name:.key, digest:{narHash:.value.narHash}, annotations:.value})[]')
+        dep=$(nix path-info "$path" --json | jq 'to_entries | map({name:.key, digest:{narHash:.value.narHash}})[]')
         narHash=$(echo "$dep" | jq -r '.digest.narHash')
         sha256="sha256:$(nix hash convert --to base16 "$narHash")"
         echo "$dep" | jq --arg sha256 "$sha256" '.digest.sha256=$sha256'
     done | jq -s .)
-
-    mkdir -p /nix/var/nix/provenance/nix/store
 
     jq -n \
         --argjson derivation "$derivation" \
@@ -47,7 +45,7 @@ writeShellApplication {
         '{
             "_type": "https://in-toto.io/Statement/v1",
             "subject": $subject,
-            "predicateType": "https://nixos.org/provenance/v1",
+            "predicateType": "https://slsa.dev/provenance/v1",
             "predicate": {
                 "buildDefinition": {
                     "buildType": "https://nixos.org/build/v1",
@@ -66,7 +64,7 @@ writeShellApplication {
                     },
                 },
             }
-        }' > "/nix/var/nix/provenance$DRV_PATH.slsa.json"
+        }' > "$(basename "$DRV_PATH").slsa.json"
 
   '';
 
